bfbaee188dd673a64c2c475a7ea37e26e499930f
Chiefautoparts <maxwelljohnson@comcast.net>
2017-03-07 14:52:23 -0800

    added new script to java file

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 'use strict';
+
+var question1 = prompt('Is there a greater sport than hockey?')
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('Great minds think alike, Hockey is indeed the greatest sport');
+} else if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') {
+  alert('WHAT! Are you crazy you get to fight people ON ICE');
+} else {
+  alert('Come on man it does\'t take rocket appliances to answer yes or no');
+}
+
+var q2 = prompt('')
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase('') === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var q2 = prompt('')
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase('') === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var q2 = prompt('')
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase('') === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var q2 = prompt('')
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase('') === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
+
+var q2 = prompt('')
+if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') {
+  alert('');
+} else if (answer.toLowerCase('') === 'no' || answer.toLowerCase() === 'n') {
+  alert('');
+} else {
+  alert('');
+}
